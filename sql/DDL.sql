e82499ec87a8b149967af7175abff2e64153c59c
saito <saito@e-grant.net>
2013-09-13 15:59:11 +0900

    DML文の不足分を追加

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -1,11 +1,14 @@
-CREATE TABLE thread_lists (
+CREATE DATABASE db01 CHARACTER SET utf8
+
+GRANT SELECT ,INSERT ,UPDATE ,DELETE ON db01.* TO user01@"localhost" IDENTIFIED BY "user01";
+
+CREATE TABLE db01.thread_lists (
 	id int NOT NULL AUTO_INCREMENT,
-	title varchar(255) NOT NULL,
-	maker varchar(255) NOT NULL,
+	title varchar(255) NOT NULL
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE thread_contents (
+CREATE TABLE db01.thread_contents (
 	id int NOT NULL AUTO_INCREMENT,
 	thread_list_id int NOT NULL,
 	writer varchar(255) NOT NULL,
@@ -13,5 +16,5 @@ CREATE TABLE thread_contents (
 	writetext text NOT NULL,
 	viewflg boolean NOT NULL DEFAULT TRUE,
 	PRIMARY KEY(id),
-	FOREIGN KEY(thread_lists_id) REFERENCES thread_lists(id)
+	FOREIGN KEY(thread_list_id) REFERENCES thread_lists(id)
 );
